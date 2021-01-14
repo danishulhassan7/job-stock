9cb90aaa1327d0b493dbd96013086b8314b9828e
danishulhassan7 <danishulhassan7@gmail.com>
2021-01-14 14:20:36 +0500

    Job-Stock >> Footer: Adding a Footer comp

---
+++ job-stock/src/components/Footer.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import '../App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row, Col, Tabs, Tab } from 'react-bootstrap';
+import Styles from './Styles';
+
+function Footer() {
+    return(
+        <Styles>
+        </Styles>
+    );
+}
+
+export default Footer();
