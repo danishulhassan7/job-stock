f7d94fd09e2245494f18b61a220f3031336d82c8
danishulhassan7 <danishulhassan7@gmail.com>
2021-01-14 14:31:06 +0500

    Job-Stock >> Footer: Adding a Route (Removing Errors)

--- job-stock/src/components/Footer.js
+++ job-stock/src/components/Footer.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import '../App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Row, Col, Tabs, Tab } from 'react-bootstrap';
@@ -7,9 +7,9 @@ import Styles from './Styles';
 function Footer() {
     return(
         <Styles>
-            hello
+            hello World
         </Styles>
     );
 }
 
-export default Footer();
+export default Footer;
