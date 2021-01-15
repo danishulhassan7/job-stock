afbd0b6ae6f59ea723a4abd5cf7cf357b856a386
danishulhassan7 <danishulhassan7@gmail.com>
2021-01-15 22:44:42 +0500

    Job-Stock >> Added Footer.css

--- job-stock/src/components/Footer.js
+++ job-stock/src/components/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../App.css';
+import '../Footer.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Row, Col} from 'react-bootstrap';
 import Styles from './Styles';
