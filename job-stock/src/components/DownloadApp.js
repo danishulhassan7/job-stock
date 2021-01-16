05b5bbcb1b136f7b95d7758199fbac2a4df7e155
danishulhassan7 <danishulhassan7@gmail.com>
2021-01-16 22:40:25 +0500

    Job-Stock >> DownloadApp: Comp Added

---
+++ job-stock/src/components/DownloadApp.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Styles from './Styles';
+import { Container, Row, Col } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+export const DownloadApp = (props) => (
+    
+);
+
+
+
