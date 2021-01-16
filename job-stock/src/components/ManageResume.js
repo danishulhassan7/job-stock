1239abee3c95004c88a471d7d472dc7b4e13966b
danishulhassan7 <danishulhassan7@gmail.com>
2021-01-16 18:45:52 +0500

    Job-Stock >> Manage Resume Comp: Props

--- job-stock/src/components/ManageResume.js
+++ job-stock/src/components/ManageResume.js
@@ -9,10 +9,10 @@ import client2 from '../assets/img/client-2.jpg';
 import client3 from '../assets/img/client-3.jpg';
 import client4 from '../assets/img/client-4.jpg';
 import client5 from '../assets/img/client-5.jpg';
-import { BrowsResComp } from './BrowsResComp';
 import { BrowsResForm } from './BrowsResForm';
 import { PaginationComp } from './PaginationComp';
 import Footer from './Footer';
+import { MngRsmCmp } from './MngRsmCmp';
 
 function ManageResume() {
     return (
@@ -30,46 +30,24 @@ function ManageResume() {
             {/* Adding a BrowsResComp Component  */}
 
             <Container>
-                <BrowsResForm />
+                <MngRsmCmp
+                    img = {client1}
+                    name = "Alexander Weir"
+                    title = "Web Developer"
+                    rate = "$19/hour"
+                    active = "12 hours ago"
+                    sk1 = "HTML"
+                    sk2 = "CSS"
+                    sk3 = "JS"
+                    sk4 = "WordPress"
+                />
             </Container>
             <br />
-            {/* Browse Resume Section  */}
-            <Container>
-                <article>
-                    <div className="mng-resume">
-                        <Row>
-                                <Col md={2} sm={2}>
-                                    <div className="mng-resume-pic">
-                                        <img src={client1} className="img-responsive" alt="" />
-                                    </div>
-                                </Col>
-                                <Col md={3} sm={3}>
-                                    <div className="mng-resume-name">
-                                        <h4>Alexander Weir <span className="cand-designation">(Web Developer)</span></h4>
-                                        <span className="cand-status">10 Hour Ago</span>
-                                    </div>
-                                </Col>
-                                <Col md={2} sm={2}>
-                                    <div className="per-hour-rate">
-                                        <p><i className="fa fa-money"></i> $17/Hour</p>
-                                    </div>
-                                </Col>
-                                <Col md={4} sm={4}>
-                                    <div className="mng-employee-skill">
-                                        <span>html</span><span>css</span><span>java</span>
-                                        <span>bootstrap</span>
-                                    </div>
-                                </Col>
-                                <Col md={1} sm={1}>
-                                    <div className="mng-resume-action">
-                                        <a href="#" data-toggle="tooltip" title="Edit"><i className="fa fa-edit"></i></a>
-                                        <a href="#" data-toggle="tooltip" title="Delete"><i className="fa fa-trash-o"></i></a>
-                                    </div>
-                                </Col>
-                        </Row>
-                    </div>
-                </article>
-            </Container>
+
+            {/* Browse Resume Section Components  */}
+
+            <MngRsmCmp/>
+
             <br />
             <PaginationComp />
             <br /> <br />
